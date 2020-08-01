411612dbcb35036588ba83321bbf318444741cf8
ait <ait@ex.in>
2020-08-02 04:55:27 +0900

    [Comp] add set

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+class Counter extends Component {
+    render() {
+      return (
+        <>
+          <h1>Counter</h1>
+        </>
+      )
+    }
+}
+
+export default Counter
