67e8c531204d873fa5f4579399f7125968f9f739
ait <ait@ex.in>
2020-08-02 05:09:46 +0900

    [State] setState add count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react"
 class Counter extends Component {
-    render() {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  inc = () =>{
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render() {
       return (
         <>
-          <h1>Counter</h1>
+          <h1>count</h1>
+          <p>{this.state.count}</p>
+          <button onClick={this.inc}>Add</button>
         </>
-      )
-    }
+      ) }
 }
 
 export default Counter
